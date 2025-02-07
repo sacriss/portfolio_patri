ae3f4ab28dc80dde80c32b793f13cc1a5cab1613
sacriss <sacramentos.subiela@gmail.com>
2025-02-07 14:47:54 +0100

    subiendo cambios

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const contactButton = document.querySelector(".btn");
+    
+    contactButton.addEventListener("click", () => {
+        alert("Contact form coming soon!");
+    });
+});
